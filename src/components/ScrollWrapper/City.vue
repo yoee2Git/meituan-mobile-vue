75163b12a12681c7eb3232dd1a84ca28338af7f0
yoee <yoeex@outlook.com>
2020-04-02 18:09:26 +0800

    封装better-scroll

--- src/components/ScrollWrapper/City.vue
+++ src/components/ScrollWrapper/City.vue
@@ -1,29 +1,34 @@
 <template>
-	<div class="scroll-wrapper" ref="wrapper">
+  <!-- <div class="scroll-wrapper" ref="wrapper">
 		<div class="scroll-conetent">
 			<current-city></current-city>
 			<city-list></city-list>
 		</div>
-	</div>
+  </div>-->
+  <bscroll>
+    <current-city></current-city>
+    <city-list></city-list>
+  </bscroll>
 </template>
 
 <script>
-  import BetterScroll from 'better-scroll';
-  import CurrentCity from './CityList/CurrentCity';
-  import CityList from './CityList';
+// import BetterScroll from 'better-scroll';
+import bscroll from "@/assets/components/comon/BScroll";
+import CurrentCity from "./CityList/CurrentCity";
+import CityList from "./CityList";
 
-	export default {
-		name: 'CityScrollWrapper',
-		components: {
-      CurrentCity,
-      CityList
-		},
-		mounted () {
-			this.scroll = new BetterScroll(this.$refs.wrapper);
-		}
-	}
+export default {
+  name: "CityScrollWrapper",
+  components: {
+    CurrentCity,
+    CityList,
+    bscroll
+  },
+  // mounted() {
+  //   this.scroll = new BetterScroll(this.$refs.wrapper);
+  // }
+};
 </script>
 
 <style>
-	
 </style>
---
+++ src/components/comon/BScroll.vue
@@ -0,0 +1,31 @@
+<template>
+  <div ref='wrapper'>
+   <div class="content">
+     <slot></slot>
+   </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll';
+
+export default {
+  name: bscroll,
+  components: {
+    data(){
+      return{
+        bscroll:null
+      }
+    }
+  },
+  mounted(){
+    this.bscroll = new BScroll(this.$refs.wrapper,{
+
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
